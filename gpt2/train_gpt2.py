ebb24721b398f69bde4aa98cbf1992ab60df7aa2
Arslan Kazmi <arslanahmadkazmi@gmail.com>
2026-01-05 17:02:53 +0500

    moved gpt files to their own directory and started gpt2 implementation

---
+++ gpt2/train_gpt2.py
@@ -0,0 +1,97 @@
+from dataclasses import dataclass
+import math
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class CausalSelfAttention(nn.Module):
+    
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        # key, query, value projections for all heads
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+
+        # not really a bias but more of a mask
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+        
+
+    def forward(self, x):
+
+        B, T, C = x.size()
+
+        qkv = self.c_attn(x) # (B, T, 3 * C)
+        q, k, v = qkv.split(self.n_embed, dim=2) # each is (B, T, C)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        att = ( q @ k.transpose(-2, -1) ) * ( 1.0 / math.sqrt(k.size(-1)) ) 
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+
+        y = att @ v # (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # avengers re-assemble
+
+        # output projection
+        y = self.c_proj(y)
+        return y
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x - self.c_proj(x)
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embed)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embed)
+        self.mlp = MLP(config) # analogous to FeedForward
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x)) # x + provides a residual connection stream
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embed: int = 384
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            # wte - weights of token embeddings
+            wte = nn.Embedding(config.vocab_size, config.n_embed),
+            # wpe - weights of position embeddings
+            wpe = nn.Embedding(config. block_size, config.n_embed),
+            # h for hidden layers
+            h = nn.ModuleList([ Block(config) for _ in range(config.n_layer) ]),
+            ln_f = nn.LayerNorm(config.n_embed),
+        ))
+
+        self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
