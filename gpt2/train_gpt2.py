bbbd977321b3377502fc2c747b5ff6dc6b2c1545
Arslan Kazmi <arslanahmadkazmi@gmail.com>
2026-01-05 17:04:15 +0500

    config defaults now match gpt2

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -71,11 +71,11 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256
-    vocab_size: int = 65
-    n_layer: int = 6
-    n_head: int = 6
-    n_embed: int = 384
+    block_size: int = 1024
+    vocab_size: int = 50257 # tokens = 50K BPE merges + 256 bytes tokens + 1 for end of sequence
+    n_layer: int = 12
+    n_head: int = 12
+    n_embed: int = 768
 
 
 class GPT(nn.Module):
