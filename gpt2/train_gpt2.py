9b96317a5d2cf8851e50190e5c7ce69749910480
Arslan Kazmi <arslanahmadkazmi@gmail.com>
2026-01-05 17:50:02 +0500

    gpt2 model now loading successfully

--- gpt2/train_gpt2.py
+++ gpt2/train_gpt2.py
@@ -72,7 +72,7 @@ class Block(nn.Module):
 @dataclass
 class GPTConfig:
     block_size: int = 1024
-    vocab_size: int = 50257 # tokens = 50K BPE merges + 256 bytes tokens + 1 for end of sequence
+    vocab_size: int = 50257 # tokens = 50K BPE merges + 256 bytes tokens + 1 for end of text delineation
     n_layer: int = 12
     n_head: int = 12
     n_embed: int = 768
@@ -95,3 +95,63 @@ class GPT(nn.Module):
         ))
 
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Load a GPT model from the Hugging Face transformers library."""
+
+        assert model_type in ['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'], "Unsupported model type"
+        from transformers import GPT2LMHeadModel
+
+        print(f"Loading pretrained model {model_type} from Hugging Face...")
+        
+        config_args = {
+            "gpt2" : dict(n_layer=12, n_head=12, n_embed=768),
+            "gpt2-medium" : dict(n_layer=24, n_head=16, n_embed=1024),
+            "gpt2-large" : dict(n_layer=36, n_head=20, n_embed=1280),
+            "gpt2-xl" : dict(n_layer=48, n_head=25, n_embed=1600),
+        }[model_type]
+
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+
+        sd = model.state_dict()
+
+        sd_keys = sd.keys()
+
+        sd_keys = [ k for k in sd_keys if not k.endswith('attn.bias') ]
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+
+        sd_hf = model_hf.state_dict()
+
+        # now copy over all tensors to our own state dict
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [ k for k in sd_keys_hf if not k.endswith('attn.masked_bias') ]
+        sd_keys_hf = [ k for k in sd_keys_hf if not k.endswith('attn.bias') ]
+        
+        # these weights are stored transposed in HF transformers
+        transposed = [ 'attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+
+        assert len(sd_keys) == len(sd_keys_hf), "State dict keys length mismatch"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t()) # transposition
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+    
+
+# -------------------
+
+model = GPT.from_pretrained('gpt2')
+print("Model loaded successfully.")
